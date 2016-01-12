03123895dd8a8f80924521cf9e8a6af95996f990
vmosley <vmosley@icstars.org>
2016-01-12 02:25:45 -0600

    Renamed Package to scratchedUP. Random testing things.

--- app/src/main/java/com/newtime/scratchedUP/Animation.java
+++ app/src/main/java/com/newtime/scratchedUP/Animation.java
@@ -3,6 +3,8 @@ package com.newtime.scratchedUP;
 import android.graphics.Bitmap;
 //The only reason this is here is to test out branchA on github
 //nope
+
+//Change made to development branch
 public class Animation {
     //I think i understand this.
     private Bitmap[] frames;
